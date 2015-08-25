97302b76b364af45a13dda37e9da43cf96ae3689
Eliodor <Nikizver@bigmir.net>
2015-08-25 23:19:15 +0300

    Third

--- Task 2/Form1.Designer.cs
+++ Task 2/Form1.Designer.cs
@@ -375,6 +375,7 @@
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Календарь";
